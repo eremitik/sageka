f8231caf76cd9874ce504b20121c0c9ee3f0a9a6
Mikio Crosby <eremitik@Mikios-MacBook-Air.local>
2021-07-26 21:57:56 +0900

    added redux, basic styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import shinzo from "./images/shinzo_abe.jpg";
-import Persons from "./components/Persons/Persons";
+import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import shinzo from "./images/shinzo_abe.jpg";
+import useStyles from "./styles";
 
 export default function App() {
+  const classes = useStyles();
+
   return (
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Shinzo Abe</Typography>
-        <img src={shinzo} alt="memories"/>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Shinzo Abe</Typography>
+        <img className={classes.image} src={shinzo} alt="memories" height="60" />
 
 
 
@@ -18,7 +21,7 @@ export default function App() {
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Persons />
+              <Posts />
             </Grid>
             <Grid item xs={12} sm={4}>
               <Form />
--- client/src/components/Persons/Person/Person.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Person = () => {
-  return (
-    <h1>Person</h1>
-  )
-}
-
-export default Person;
